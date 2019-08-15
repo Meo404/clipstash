38e54ad6a8a35cb3c717b1479db4eed1a4ff9339
Maurice Vogel <meo404@github.com>
2019-08-15 13:31:03 +0200

    removed respond_to :json from ApiController as it's not supported by rails anymore. ActsAsApiRequest json check will suffice

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,9 +2,7 @@ module Api
   module V1
     class ApiController < ApiController
       include Api::Concerns::ActAsApiRequest
-
       layout false
-      respond_to :json
 
       rescue_from Exception,                           with: :render_error
       rescue_from ActiveRecord::RecordNotFound,        with: :render_not_found
