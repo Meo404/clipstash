d64ae20a1d108376b6388d35f1496a262ae9c14d
Maurice Vogel <meo404@github.com>
2019-08-19 11:57:20 +0200

    readded annotations

--- app/serializers/subreddit_serializer.rb
+++ app/serializers/subreddit_serializer.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: subreddits
+#
+#  id                    :bigint           not null, primary key
+#  reddit_fullname       :string
+#  display_name          :string
+#  display_name_prefixed :string
+#  public_description    :text
+#  subscribers           :integer
+#  icon_image            :string
+#  icon_size             :integer          is an Array
+#  banner_image          :string
+#  banner_size           :integer          is an Array
+#  over18                :boolean
+#  created_utc           :datetime
+#  url                   :string
+#  status_cd             :integer
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#
+
 class SubredditSerializer < ActiveModel::Serializer
   attributes :reddit_fullname,
              :display_name,
--- spec/factories/subreddits.rb
+++ spec/factories/subreddits.rb
@@ -9,11 +9,11 @@
 #  public_description    :text
 #  subscribers           :integer
 #  icon_image            :string
-#  icon_size             :string
+#  icon_size             :integer          is an Array
 #  banner_image          :string
-#  banner_size           :string
+#  banner_size           :integer          is an Array
 #  over18                :boolean
-#  created_utc           :date
+#  created_utc           :datetime
 #  url                   :string
 #  status_cd             :integer
 #  created_at            :datetime         not null
