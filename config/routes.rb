754c120c691126b04228d4c10654aca6cc614994
Maurice Vogel <meo404@github.com>
2019-08-19 12:59:59 +0200

    working on specs

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Heartbeat Route
   get 'appInfo', to: proc { [200, {}, ['Application is running']] }
 
-  namespace :api do
+  namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       resources :subreddits
     end
--- spec/controllers/api/v1/subreddits_controller_spec.rb
+++ spec/controllers/api/v1/subreddits_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::SubredditsController, type: :controller do
+  before(:each) do
+    request.headers["accept"] = 'application/json'
+    3.times { create(:subreddit) }
+  end
 
+  describe '#index' do
+    subject { get :index, as: :json }
+
+    it { is_expected.to be_successful }
+
+    it 'returns valid json' do
+      body = JSON.parse(subject.body)
+      expect(body['data'].length).to eq(3)
+    end
+  end
 end
