912483799edd561b222f2bdd12a235d7bec72fb9
Meo404 <meo404@github.com>
2019-09-16 10:27:15 +0200

    added scrollToTop hook that automatically scrolls to the top of the page on route change

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { createMuiTheme, MuiThemeProvider, makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Navigation from './Navigation/Navigation';
+import ScrollToTop from '../utils/scrollToTop';
 import SubredditGrid from "./Subreddits/SubredditGrid";
 import SubmissionGrid from './Submissions/SubmissionGrid';
 
@@ -12,14 +13,16 @@ export default function App() {
     return (
         <MuiThemeProvider theme={theme}>
             <Router>
-                <div className={classes.root}>
-                    <CssBaseline />
-                    <Navigation />
-                    <Switch>
-                        <Route path='/subreddits' exact component={SubredditGrid} />
-                        <Route path='/r/:displayName' component={SubmissionGrid} />
-                    </Switch>
-                </div>
+                <ScrollToTop>
+                    <div className={classes.root}>
+                        <CssBaseline />
+                        <Navigation />
+                        <Switch>
+                            <Route path='/subreddits' exact component={SubredditGrid} />
+                            <Route path='/r/:displayName' component={SubmissionGrid} />
+                        </Switch>
+                    </div>
+                </ScrollToTop>
             </Router>
         </MuiThemeProvider>
     )
