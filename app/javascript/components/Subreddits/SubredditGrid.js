3279eb9a7d4dc16308bc3767414a93dd08c353b3
Meo404 <meo404@github.com>
2019-09-17 09:45:04 +0200

    added loading indicator also to subredditgrid

--- app/javascript/components/Subreddits/SubredditGrid.js
+++ app/javascript/components/Subreddits/SubredditGrid.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import axios from "axios";
+import LoadingIndicator from '../UI/LoadingIndicator';
 import SubredditGridItem from "./SubredditGridItem";
 import MaxWidthContainer from "../../hoc/MaxWidthContainer";
 
+
 export default function SubredditGrid() {
-    const [data, setData] = useState({subreddits: []});
+    const [data, setData] = useState({ subreddits: [], isLoading: true });
 
     useEffect(() => {
         fetchData();
@@ -13,11 +15,17 @@ export default function SubredditGrid() {
 
     async function fetchData() {
         const result = await axios('/api/v1/subreddits');
-        setData({subreddits: result.data.subreddits});
+        setData({subreddits: result.data.subreddits, isLoading: false});
+    }
+
+    let loadingIndicator = null;
+    if (data.isLoading) {
+        loadingIndicator = <LoadingIndicator key='loadingIndicator'/>
     }
 
     return (
         <MaxWidthContainer>
+                {loadingIndicator}
                 <Grid container spacing={0}>
                     {data.subreddits.map((subreddit) => (
                         <SubredditGridItem subreddit={subreddit} key={subreddit.id} />
