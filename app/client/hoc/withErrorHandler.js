7f7e5cabcc775cf7b49c263fdab96e0663aa4eb0
Meo404 <mvogel.privat@gmail.com>
2019-09-22 12:22:34 +0200

    added basic errorHandler HOC

---
+++ app/client/hoc/withErrorHandler.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+const withErrorHandler = (WrappedComponent) => {
+    return props => {
+        const responseInterceptor = axios.interceptors.response.use(
+            response => res,
+            error => {
+                console.log(error);
+            }
+        );
+
+        useEffect(() => {
+            return () => {
+                axios.interceptors.response.eject(responseInterceptor);
+            };
+        }, [responseInterceptor]);
+
+        return <WrappedComponent {...props} />;
+    };
+};
+
+export default withErrorHandler;
