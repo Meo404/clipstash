a42bb91e1b2467742519515a6c080e53f9283fc0
Meo404 <meo404@github.com>
2019-09-25 10:47:14 +0200

    added pretty_location plugin for better folder structure of images

--- app/uploaders/thumbnail_uploader.rb
+++ app/uploaders/thumbnail_uploader.rb
@@ -1,10 +1,12 @@
 require "image_processing/mini_magick"
 
 class ThumbnailUploader < Shrine
-  plugin :remote_url, max_size: 20 * 1024 * 1024
   plugin :determine_mime_type
+  plugin :pretty_location
   plugin :processing
+  plugin :remote_url, max_size: 20 * 1024 * 1024
   plugin :store_dimensions, analyzer: :mini_magick
+
 
   process(:store) do |io, context|
     io.download do |original|
