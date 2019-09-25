ca11ecac42581e36a8ad491cfaf04898c81f6be5
Meo404 <meo404@github.com>
2019-09-25 12:48:53 +0200

    added test configuration to shrine. added file cleaner for tests

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,9 +1,19 @@
 require "shrine"
 require "shrine/storage/file_system"
 
-Shrine.storages = {
-    cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"),       # cache
-    store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store"),       # permanent
-}
+if Rails.env.test?
+  Shrine.storages = {
+      cache: Shrine::Storage::FileSystem.new("spec", prefix: "support/uploads/cache"),       # cache
+      store: Shrine::Storage::FileSystem.new("spec", prefix: "support/uploads/store"),       # permanent
+  }
+end
+
+if Rails.env.development?
+  Shrine.storages = {
+      cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"),       # cache
+      store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store"),       # permanent
+  }
+end
+
 
 Shrine.plugin :activerecord
