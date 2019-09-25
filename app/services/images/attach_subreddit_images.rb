6bd19a70d885ebc15e3226abe7ba6cd37237ead5
Meo404 <meo404@github.com>
2019-09-25 17:01:43 +0200

    fixed image duplication by combining image updates and adding conditionals

--- app/services/images/attach_subreddit_images.rb
+++ app/services/images/attach_subreddit_images.rb
@@ -2,8 +2,7 @@ module Images
   # Service Class to upload and process thumbnail images for all new submissions
   class AttachSubredditImages < ApplicationService
     def call
-      update_icons
-      update_banners
+      update_images
 
       # Clean shrine cache afterwards as it doesn't happen automatically
       Shrine.storages[:cache].clear!
@@ -11,24 +10,24 @@ module Images
 
     private
 
-      def update_icons
+      def update_images
         subreddits = Subreddit
                          .where(icon_data: nil)
                          .where.not(reddit_icon: nil)
+                         .or(Subreddit
+                                 .where(banner_data: nil)
+                                 .where.not(reddit_banner: nil)
+                         )
 
         subreddits.each do |subreddit|
-          subreddit.icon_remote_url = subreddit.reddit_icon
-          subreddit.save
-        end
-      end
+          if !subreddit.reddit_icon.nil? && subreddit.icon_data.nil?
+            subreddit.icon_remote_url = subreddit.reddit_icon
+          end
 
-      def update_banners
-        subreddits = Subreddit
-                         .where(banner_data: nil)
-                         .where.not(reddit_banner: nil)
+          if !subreddit.reddit_banner.nil? && subreddit.banner_data.nil?
+            subreddit.banner_remote_url = subreddit.reddit_banner
+          end
 
-        subreddits.each do |subreddit|
-          subreddit.banner_remote_url = subreddit.reddit_banner
           subreddit.save
         end
       end
