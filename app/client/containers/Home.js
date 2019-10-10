51ef53079de1ba4f07faaa1f3e88413e6bddf6b6
Meo404 <mvogel.privat@gmail.com>
2019-10-10 22:36:31 +0200

    added loadMore button to home

--- app/client/containers/Home.js
+++ app/client/containers/Home.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { withRouter } from 'react-router-dom';
 import axios from "axios";
+import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
 import MaxWidthContainer from 'components/UI/MaxWidthContainer';
 import LoadingIndicator from 'components/UI/LoadingIndicator';
 import Typography from "@material-ui/core/Typography";
@@ -9,8 +12,11 @@ import SubmissionList from "../components/SubmissionList";
 
 function Home(props) {
     const { history } = props;
+    const classes = useStyles();
+
     const [submissions, setSubmissions] = useState({ submissions: [], isLoading: true });
     const [subreddits, setSubreddits] = useState([]);
+    const [showMore, setShowMore] = useState(false);
 
     let recommendedSubmissions;
 
@@ -20,7 +26,7 @@ function Home(props) {
     }, []);
 
     async function fetchRecommendedSubmissions() {
-        const result = await axios('/api/v1/recommended_submissions/');
+        const result = await axios('/api/v1/recommended_submissions/', { params: { max_results: 16 } });
         if (result) {
             setSubmissions({ submissions: result.data.submissions, isLoading: false });
         }
@@ -33,19 +39,33 @@ function Home(props) {
         }
     }
 
+    function showMoreHandler() {
+        setShowMore(true);
+    }
+
     if (submissions.isLoading) {
         recommendedSubmissions = <LoadingIndicator key='loadingIndicator' />;
     } else {
+        let showMoreButton = <Button onClick={showMoreHandler} size="small" className={classes.moreButton}>Show More</Button>;
+        let displayedSubmissions = submissions.submissions.slice(0, 8)
+
+        if (showMore) {
+            showMoreButton = null;
+            displayedSubmissions = submissions.submissions;
+        }
+
         recommendedSubmissions = (
             <React.Fragment>
-                <Typography variant="h6"style={{ padding: 10 }}>
+                <Typography variant="h6" className={classes.headingText}>
                     Recommended Videos
                 </Typography>
                 <SubmissionList
-                    submissions={submissions.submissions}
+                    submissions={displayedSubmissions}
                     searchState={{}}
                     history={history}
                 />
+                {showMoreButton}
+                <Divider className={classes.Divider} />
             </React.Fragment>
         )
     }
@@ -55,13 +75,14 @@ function Home(props) {
             {recommendedSubmissions}
             {subreddits.map((subreddit) => (
                 <React.Fragment key={subreddit.id}>
-                    <Typography variant="h6" style={{ padding: 10 }}>
+                    <Typography variant="h6" className={classes.headingText}>
                         Recommended from {subreddit.display_name_prefixed}
                     </Typography>
                     <SubmissionList
                         submissions={subreddit.submissions}
                         history={history}
                     />
+                    <Divider className={classes.Divider} />
                 </React.Fragment>
             ))}
         </MaxWidthContainer>
@@ -69,3 +90,28 @@ function Home(props) {
 }
 
 export default withRouter(withErrorHandler(Home));
+
+const useStyles = makeStyles(theme => ({
+    Divider: {
+        margin: "10px 0",
+        [theme.breakpoints.up('sm')]: {
+            margin: "0 10px",
+            marginTop: 20
+        },
+    },
+    moreButton: {
+        marginLeft: 0,
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: 5
+        },
+    },
+    headingText: {
+        paddingTop: 10,
+        paddingLeft: 5,
+        fontSize: "1rem",
+        [theme.breakpoints.up('sm')]: {
+            padding: 10,
+            fontSize: "1.25rem"
+        },
+    }
+}));
