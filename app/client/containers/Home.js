82d71f1d94a51e2d60fb4800af6fd39594e87881
Meo404 <meo404@github.com>
2019-10-11 10:50:01 +0200

    moved recommended submissions/subreddits into their own component

--- app/client/containers/Home.js
+++ app/client/containers/Home.js
@@ -1,24 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import { withRouter } from 'react-router-dom';
 import axios from "axios";
-import Button from "@material-ui/core/Button";
-import Divider from "@material-ui/core/Divider";
 import MaxWidthContainer from 'components/UI/MaxWidthContainer';
 import LoadingIndicator from 'components/UI/LoadingIndicator';
-import Typography from "@material-ui/core/Typography";
 import withErrorHandler from "hoc/withErrorHandler";
-import SubmissionList from "../components/SubmissionList";
+import RecommendedSubmissions from "components/RecommendedSubmissions";
+import RecommendedSubreddits from "components/RecommendedSubreddits";
 
 function Home(props) {
     const { history } = props;
-    const classes = useStyles();
-
     const [submissions, setSubmissions] = useState({ submissions: [], isLoading: true });
     const [subreddits, setSubreddits] = useState([]);
     const [showMore, setShowMore] = useState(false);
-
-    let recommendedSubmissions;
 
     useEffect(() => {
         fetchRecommendedSubmissions();
@@ -39,79 +32,33 @@ function Home(props) {
         }
     }
 
+    function displayedSubmissions() {
+        return showMore ? submissions.submissions : submissions.submissions.slice(0,8)
+    }
+
     function showMoreHandler() {
         setShowMore(true);
     }
 
+    let recommendedSubmissions = (
+        <RecommendedSubmissions 
+            submissions={displayedSubmissions()} 
+            history={history}
+            showMore={showMore}
+            showMoreHandler={showMoreHandler} 
+        />
+    )
+
     if (submissions.isLoading) {
-        recommendedSubmissions = <LoadingIndicator key='loadingIndicator' />;
-    } else {
-        let showMoreButton = <Button onClick={showMoreHandler} size="small" className={classes.moreButton}>Show More</Button>;
-        let displayedSubmissions = submissions.submissions.slice(0, 8)
-
-        if (showMore) {
-            showMoreButton = null;
-            displayedSubmissions = submissions.submissions;
-        }
-
-        recommendedSubmissions = (
-            <React.Fragment>
-                <Typography variant="h6" className={classes.headingText}>
-                    Recommended Videos
-                </Typography>
-                <SubmissionList
-                    submissions={displayedSubmissions}
-                    searchState={{}}
-                    history={history}
-                />
-                {showMoreButton}
-                <Divider className={classes.Divider} />
-            </React.Fragment>
-        )
+        recommendedSubmissions = <LoadingIndicator />;
     }
 
     return (
         <MaxWidthContainer>
             {recommendedSubmissions}
-            {subreddits.map((subreddit) => (
-                <React.Fragment key={subreddit.id}>
-                    <Typography variant="h6" className={classes.headingText}>
-                        Recommended from {subreddit.display_name_prefixed}
-                    </Typography>
-                    <SubmissionList
-                        submissions={subreddit.submissions}
-                        history={history}
-                    />
-                    <Divider className={classes.Divider} />
-                </React.Fragment>
-            ))}
+            <RecommendedSubreddits subreddits={subreddits} history={history} />
         </MaxWidthContainer>
     );
 }
 
 export default withRouter(withErrorHandler(Home));
-
-const useStyles = makeStyles(theme => ({
-    Divider: {
-        margin: "10px 0",
-        [theme.breakpoints.up('sm')]: {
-            margin: "0 10px",
-            marginTop: 20
-        },
-    },
-    moreButton: {
-        marginLeft: 0,
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: 5
-        },
-    },
-    headingText: {
-        paddingTop: 10,
-        paddingLeft: 5,
-        fontSize: "1rem",
-        [theme.breakpoints.up('sm')]: {
-            padding: 10,
-            fontSize: "1.25rem"
-        },
-    }
-}));
