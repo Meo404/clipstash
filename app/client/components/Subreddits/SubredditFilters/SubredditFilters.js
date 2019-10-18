9ac2edd1e86605b32ce478a480f476e62f8ac878
Meo404 <meo404@github.com>
2019-10-18 10:44:59 +0200

    fixed icon display on subreddit header. started implementing proper Subreddits Header

--- app/client/components/Subreddits/SubredditFilters/SubredditFilters.js
+++ app/client/components/Subreddits/SubredditFilters/SubredditFilters.js
@@ -1,15 +1,49 @@
-import React from 'react';
-import { SubredditSortMethods } from 'constants/SortMethods';
+import React from "react";
+import { SubredditSortMethods } from "constants/SortMethods";
+import { 
+    Card,
+    CardActions,
+    CardContent,
+    Divider,
+    Hidden,
+    Typography
+} from "@material-ui/core";
 import { SortDropDown } from "components";
+
+import useStyles from "./Styles";
 
 export default function SubredditFilters(props) {
     const { sortChangeHandler, sortMethod } = props;
+    const classes = useStyles();
 
     return (
-        <SortDropDown
-            selectedMethod={sortMethod}
-            sortChangeHandler={sortChangeHandler}
-            sortMethods={SubredditSortMethods}
-         />
+        <React.Fragment>
+            <Card className={classes.card}>
+                <div className={classes.details}>
+                    <CardContent className={classes.content}>
+                        <Typography variant="h6" className={classes.titleText}>
+                            Subreddits
+                    </Typography>
+                        <Typography
+                            variant="subtitle1"
+                            color="textSecondary"
+                            className={classes.descriptionText}
+                        >
+                            Find your favorite subreddit and start browsing videos!
+                        </Typography>
+                    </CardContent>
+                </div>
+                <CardActions className={classes.sortDropDown} >
+                    <SortDropDown
+                        selectedMethod={sortMethod}
+                        sortMethods={SubredditSortMethods}
+                        sortChangeHandler={sortChangeHandler}
+                    />
+                </CardActions>
+            </Card >
+            <Hidden smDown>
+                <Divider className={classes.divider} />
+            </Hidden>
+        </React.Fragment>
     )
 }
