9017d604f7e4b8319440650421a748b061afc79b
Meo404 <mvogel.privat@gmail.com>
2019-10-21 21:25:46 +0200

    moved and adjusted specs for find_submissions service

--- app/services/submissions/search/base_search.rb
+++ app/services/submissions/search/base_search.rb
@@ -1,6 +1,6 @@
 module Submissions
   module Search
-    # Interface encapsulating commong search functions
+    # Interface encapsulating commong search functionalities
     class BaseSearch < ApplicationService
       def initialize(sort_param)
         @sort_param = sort_param
@@ -18,29 +18,29 @@ module Submissions
 
       private
 
-      # These method will be defined by the classes children later on
-      def hot_submissions; end
-      def top_submissions; end
-      def filtered_submissions; end
+        # These methods will be defined by the classes children later on
+        def hot_submissions; end
+        def top_submissions; end
+        def filtered_submissions; end
 
-      def set_sort_method(sort_param)
-        VALID_SORT_PARAMS.exclude?(sort_param) ? "hot" : sort_param
-      end
-
-      def extract_date(sort_param)
-        case sort_param
-        when "top_day"
-          1.day.ago.in_time_zone("UTC")
-        when "top_week"
-          1.week.ago.in_time_zone("UTC")
-        when "top_month"
-          1.month.ago.in_time_zone("UTC")
-        when "top_year"
-          1.year.ago.in_time_zone("UTC")
-        else
-          1.week.ago.in_time_zone("UTC")
+        def set_sort_method(sort_param)
+          VALID_SORT_PARAMS.exclude?(sort_param) ? "hot" : sort_param
         end
-      end
+
+        def extract_date(sort_param)
+          case sort_param
+          when "top_day"
+            1.day.ago.in_time_zone("UTC")
+          when "top_week"
+            1.week.ago.in_time_zone("UTC")
+          when "top_month"
+            1.month.ago.in_time_zone("UTC")
+          when "top_year"
+            1.year.ago.in_time_zone("UTC")
+          else
+            1.week.ago.in_time_zone("UTC")
+          end
+        end
     end
   end
 end
