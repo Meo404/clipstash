73be497b8ebdeca651c66e662afb2e54385a1099
Meo404 <meo404@github.com>
2019-10-22 13:18:50 +0200

    also moved user_agent to credentials as to comply with reddits api rules

--- lib/redd_wrapper/session.rb
+++ lib/redd_wrapper/session.rb
@@ -12,7 +12,7 @@ module ReddWrapper
 
       def initialize_session
         Redd.it(
-          user_agent: "Project-Free Development",
+          user_agent: Rails.application.credentials.reddit[:user_agent],
           client_id: Rails.application.credentials.reddit[:client_id],
           secret: Rails.application.credentials.reddit[:secret],
           redirect_url: "http://localhost:3000"
