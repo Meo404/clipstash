38e66cb510f7e706be2bee04bcafc79bb877a61c
Meo404 <meo404@github.com>
2019-12-03 16:34:22 +0100

    improved create spec

--- spec/requests/api/v1/favorite_submissions/create_spec.rb
+++ spec/requests/api/v1/favorite_submissions/create_spec.rb
@@ -6,19 +6,38 @@ describe 'POST api/v1/favorite_submissions', type: :request do
       create(:submission, subreddit: create(:subreddit))
     end
 
-    let(:user) { User.first }
-    let(:auth_headers) { user.create_new_auth_token }
-    let(:params) {{ submission_fullname: Submission.first.reddit_fullname }}
+    context 'with valid params' do
+      let(:user) { User.first }
+      let(:auth_headers) { user.create_new_auth_token }
+      let(:params) {{ submission_fullname: Submission.first.reddit_fullname }}
 
-    it 'returns success response' do
-      post "/api/v1/favorite_submissions", params: params, headers: auth_headers, as: :json
-      expect(response).to have_http_status(:success)
+      it 'returns success response' do
+        post "/api/v1/favorite_submissions", params: params, headers: auth_headers, as: :json
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'adds submission to user favorites' do
+        expect {
+          post "/api/v1/favorite_submissions", params: params, headers: auth_headers, as: :json
+        }.to change { user.submissions.count }.by(1)
+      end
     end
 
-    it 'adds submission to user favorites' do
-      expect {
+    context 'with invalid params' do
+      let(:user) { User.first }
+      let(:auth_headers) { user.create_new_auth_token }
+      let(:params) {{ submission_fullname: 'some other string' }}
+
+      it 'returns not found response' do
         post "/api/v1/favorite_submissions", params: params, headers: auth_headers, as: :json
-      }.to change { user.submissions.count }.by(1)
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it 'does not change user favorite count' do
+        expect {
+          post "/api/v1/favorite_submissions", params: params, headers: auth_headers, as: :json
+        }.to_not change { user.submissions.count }
+      end
     end
   end
 
