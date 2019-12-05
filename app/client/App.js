100811771450b9fc42fb44ed5b39e7a474e90520
Meo404 <mvogel.privat@gmail.com>
2019-12-05 20:33:01 +0100

    minor cleanup

--- app/client/App.js
+++ app/client/App.js
@@ -1,10 +1,10 @@
 import React from "react"
 import { makeStyles } from "@material-ui/core/styles";
 import { Route, Switch } from "react-router-dom";
-import ProtectedRoute from 'hoc/ProtectedRoute';
 import AppNavigation from "containers/AppNavigation";
 import Home from "containers/Home";
 import NotFound from "components/NotFound";
+import ProtectedRoute from 'hoc/ProtectedRoute';
 import SavedVideos from "containers/SavedVideos";
 import Submission from "containers/Submission";
 import Subreddit from "containers/Subreddit";
