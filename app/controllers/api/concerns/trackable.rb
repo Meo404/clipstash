07b3cd912d505c9fb20bdf5d2d490143663a713c
Meo404 <meo404@github.com>
2019-12-12 12:12:24 +0100

    added more events

--- app/controllers/api/concerns/trackable.rb
+++ app/controllers/api/concerns/trackable.rb
@@ -14,8 +14,31 @@ module Api
 
         def track_subreddit_view
           ahoy.track "subreddit_view",
-              subreddit_id: @subreddit.id,
-              display_name: @subreddit.display_name
+                     subreddit_id: @subreddit.id,
+                     display_name: @subreddit.display_name
+        end
+
+        def track_submission_view
+          ahoy.track "submission_view",
+                     submission_fullname: @submission.reddit_fullname,
+                     subreddit_id: @submission.subreddit_id,
+                     slug: @submission.slug
+        end
+
+        def track_favorite_submissions_view
+          ahoy.track "favorite_submissions_view",
+                     pagination_page: params[:page],
+                     pagination_max_results: params[:max_results]
+        end
+
+        def track_favorite_submission_added
+          ahoy.track "favorite_submission_added",
+                     submission_fullname: params[:submission_fullname]
+        end
+
+        def track_favorite_submission_removed
+          ahoy.track "favorite_submission_removed",
+                     submission_fullname: params[:submission_fullname]
         end
     end
   end
